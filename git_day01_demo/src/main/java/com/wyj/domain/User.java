abbca738905be476349edbcc85697d3d4790f5db
wyj <18271958715@gmail.com>
2019-01-04 15:18:03 +0800

    初步开发

---
+++ git_day01_demo/src/main/java/com/wyj/domain/User.java
@@ -0,0 +1,13 @@
+package com.wyj.domain;
+
+/**
+ * @ClassName User
+ * @Author WYJ
+ * @Description TODO
+ * @Date 2019/1/4 15:09
+ * @Company 墨子谷
+ * @Version V1.0
+ */
+public class User {
+    private  Integer id;
+}
---
+++ git_day01_demo/src/main/java/com/wyj/uyil/Util.java
@@ -0,0 +1,15 @@
+package com.wyj.uyil;
+
+/**
+ * @ClassName Util
+ * @Author WYJ
+ * @Description TODO
+ * @Date 2019/1/4 15:10
+ * @Company 墨子谷
+ * @Version V1.0
+ */
+public class Util {
+    public static void main(String[] args) {
+        System.out.println("吴永俊");
+    }
+}
